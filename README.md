3b9cbb2013a1b993cf3e6c791733adfb42f9cdd7
Axel Truta <axeltruta803@gmail.com>
2025-12-20 13:05:09 +0100

    Image update daily

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# random-game-cover
-A simple python program that choose a new video game cover each day and update it in the repository
+# Random video game cover
+A little Python program i made to have a new game cover image each day. The image can be used for anything like a README file or a website.
+
+The cover update itself each day at 8h UTC
+
+Highly inspired by @Yougo-rgb and his <a href="https://github.com/Yougo-rgb/random-code-error">random coding error</a>
+
+### game of the day :
+
+<img height="300" width="auto" alt="Random video game cover" src="https://raw.githubusercontent.com/StroyII/random-game-cover/main/cover/cover.png">
+
+
+
+# 
+
+Made by using the <a href="https://rawg.io/apidocs">RAWG api</a>
+
---
+++ generate_image.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+from io import BytesIO
+import requests
+import random
+import math
+import os
+
+# Environment
+load_dotenv()
+API_KEY = os.getenv("RAWG_API_KEY")
+API_URL = "https://api.rawg.io/api"
+PAGE_SIZE = 40
+FILENAME = "cover.png"
+
+def getGamePage(url:str,key:str,page:int=1,numerByPage:int=1)->dict:
+    """
+    Function to fetch the RAWG api
+    """
+    complete_url = url + "/games?key=" + key
+    response = requests.get(complete_url, {"page":page,"page_size":numerByPage})
+    data = response.json()
+
+    return data
+
+def saveImagePng(image_url:str, filename:str, game_name:str):
+    """
+    Function to save an image in png 
+    """
+    response = requests.get(image_url)
+
+    image = Image.open(BytesIO(response.content)).convert("RGBA")
+
+    overlay = Image.new("RGBA", image.size, (0, 0, 0, 0))
+    overlay_draw = ImageDraw.Draw(overlay)
+
+    band_height = 120
+    overlay_draw.rectangle([(0, image.height - band_height), (image.width, image.height)],fill=(0, 0, 0, 150))
+
+    image = Image.alpha_composite(image, overlay)
+    draw = ImageDraw.Draw(image)
+
+    try:
+        font = ImageFont.truetype("fonts/comicsansms.ttf", 48)
+    except:
+        font = ImageFont.load_default()
+
+    wrapped_name = "\n".join(textwrap.wrap(game_name, width=30))
+
+    draw.text((40, image.height - band_height + 30),wrapped_name,font=font,fill=(255, 255, 255, 255))
+
+    image.save("cover/"+filename, "PNG")
+
+
+# Choose a random page in the api
+numberOfGame = getGamePage(API_URL,API_KEY)["count"]
+total_page = math.ceil(numberOfGame / PAGE_SIZE)
+random_page = random.randint(1, total_page)
+
+# Fetch a random game in the page
+choosen_game = random.choice(getGamePage(API_URL,API_KEY,random_page,PAGE_SIZE)["results"])
+game_name = choosen_game["name"]
+
+# Take the cover image and save it
+image_url = choosen_game["background_image"]
+saveImagePng(image_url,FILENAME,game_name)
